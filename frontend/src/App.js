13e771880cb2241e1172eb600042a59a8b584b6b
buddha <sahasubhajit314@gmail.com>
2024-09-17 20:27:48 +0530

    integrated unsplash API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search'
 import { useState } from 'react';
-// const handleSearchSubmit = (e) => {
-//   e.preventDefault();
-// console.log(e.target[0].value);
-// console.log(e.target[1].value);
-// }
+
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
 const App = () => {
   const [word,setWord ] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
   console.log(word);
+  fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
   };
-  console.log(word);
+  // console.log(process.env.REACT_APP_UNSPLASH_KEY);
   return (
     <div>
       <Header title = "Image Gallery"/>
